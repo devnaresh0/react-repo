4a65320d3080a9c9b78b808c56c6671d02a4bcf0
Naresh Kumar <nareshkumar0000002@gmail.com>
2025-02-16 08:55:42 +0530

    commit

---
+++ 01basicvite/src/main.jsx
@@ -0,0 +1,35 @@
+import { createRoot } from "react-dom/client";
+
+import App from "./App.jsx";
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom react app</h1>
+    </div>
+  );
+}
+const AnotherElement = (
+  <a href="http://google.com" target="_blank">
+    Visit Google
+  </a>
+);
+const areactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "click to visit google"
+);
+createRoot(document.getElementById("root")).render(
+    
+    
+    areactElement
+
+);
